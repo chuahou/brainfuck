3db91a147c39c6a65f5b37a6deb78eba0768edd6
Chua Hou <human@chuahou.dev>
2019-07-25 20:57:37 +0800

    Add the rest of the brainfuck operation declaration and at zero_at_127

--- include/brainfuck.hpp
+++ include/brainfuck.hpp
@@ -28,7 +28,7 @@ namespace chbrainfuck
         /**
          * @brief An array representing the brainfuck tape
          * 
-         * Each element is represented as a char, and the lenght of the tape is
+         * Each element is represented as a char, and the length of the tape is
          * specified at runtime.
          */
         char* tape;
@@ -47,29 +47,118 @@ namespace chbrainfuck
          */
         int tape_ptr;
 
+        /**
+         * @brief Whether to operate with 0 at 0 or 0 at 127.
+         * 
+         * Operating with 0 at 127 means that cells are initialized at 127, and
+         * comparison operators [ and ] compare to 127 instead of 0. This is
+         * done to emulate behaviour of interpreters having cells with range
+         * [-127, 128].
+         */
+        bool zero_at_127;
+
     public:
+
+        /* constructors and destructor */
 
         /**
          * @brief Construct a new Brainfuck object
          * 
          * Creates a new Brainfuck object with tape size set to default of
-         * chbrainfuck::DEFAULT_TAPE_SIZE.
+         * chbrainfuck::DEFAULT_TAPE_SIZE and
+         * chbrainfuck::Brainfuck::zero_at_127 disabled.
          */
         Brainfuck();
 
         /**
          * @brief Construct a new Brainfuck object
          * 
-         * Creates a new Brainfuck object with the specified tape size.
+         * Creates a new Brainfuck object with the specified tape size and
+         * chbrainfuck::Brainfuck::zero_at_127 disabled.
          * 
          * @param size tape size of the Brainfuck object
          */
         Brainfuck(int size);
 
         /**
+         * @brief Construct a new Brainfuck object
+         * 
+         * Creates a new Brainfuck object with the specified tape size and
+         * boolean determining whether of not to enable
+         * chbrainfuck::Brainfuck::zero_at_127.
+         * 
+         * @param size tape size of the Brainfuck objcet
+         * @param use_zero_at_127 whether to enable zero_at_127
+         */
+        Brainfuck(int size, bool use_zero_at_127);
+
+        /**
          * @brief Destroy the Brainfuck object
          */
-        ~Brainfuck(); 
+        ~Brainfuck();
+
+        /* accessors */
+
+        /**
+         * @brief Get the tape array
+         * 
+         * @return char* the tape array
+         */
+        char* get_tape() { return tape; }
+
+        /**
+         * @brief Get the length of the tape array
+         * 
+         * @return int length of tape array
+         */
+        int get_tape_size() { return tape_size; }
+
+        /**
+         * @brief Get the tape pointer
+         * 
+         * @return int array index of the tape pointer
+         */
+        int get_tape_ptr() { return tape_ptr; }
+
+        /* brainfuck operations */
+
+        /**
+         * @brief Moves the pointer to the right
+         * 
+         * @return true op successful
+         * @return false op failed
+         */
+        bool increment_ptr();
+
+        /**
+         * @brief Moves the pointer to the left
+         * 
+         * @return true op successful
+         * @return false op failed
+         */
+        bool decrement_ptr();
+
+        /**
+         * @brief Increments the cell pointed at by the pointer
+         */
+        void increment_cell();
+
+        /**
+         * @brief Decrements the cell pointed at by the pointer
+         */
+        void decrement_cell();
+
+        /**
+         * @brief Outputs the character in the cell pointed at by the pointer
+         */
+        void print_cell();
+
+        /**
+         * @brief Inputs a character to the cell pointed at by the pointer
+         * 
+         * @param value character to set the cell to
+         */
+        void set_cell(char value);
 
     };
 
--- src/docpage.cpp
+++ src/docpage.cpp
@@ -4,4 +4,13 @@
  * 
  * Another one. Yep. Another one. For more, go to
  * https://github.com/chuahou/brainfuck.
+ * 
+ * @section Details
+ * 
+ * Cells are 8-bit chars. Hence, the value varies between [0, 255] and not
+ * [-127, 128], and overflow is left to the user to handle.
+ * 
+ * [ and ] can be set to compare to 127 instead of 0 to emulate the behaviour
+ * of implementation with cell range [0, 255]. See
+ * chbrainfuck::Brainfuck::zero_at_127.
  */
